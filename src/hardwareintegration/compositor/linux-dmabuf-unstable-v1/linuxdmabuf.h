b364be945773c2d26c5642395d4c2e43e72c3572
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-05-15 11:25:32 +0000

    Add missing include in linuxdmabuf.h
    Needed with newer libc++

--- src/hardwareintegration/compositor/linux-dmabuf-unstable-v1/linuxdmabuf.h
+++ src/hardwareintegration/compositor/linux-dmabuf-unstable-v1/linuxdmabuf.h
@@ -44,6 +44,8 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
+#include <array>
+
 // compatibility with libdrm <= 2.4.74
 #ifndef DRM_FORMAT_RESERVED
 #define DRM_FORMAT_RESERVED           ((1ULL << 56) - 1)
